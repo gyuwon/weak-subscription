2a55814820879ef4e03f599351ffd8f49b483540
Gyuwon Yi <gyuwon@live.com>
2014-09-25 00:20:09 +0900

    s1, s2 still survived

--- WeakSubscription/Program.cs
+++ WeakSubscription/Program.cs
@@ -1,34 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
+using Xamarin.Forms;
 
 namespace WeakSubscription
 {
-    public class Messenger
-    {
-        public static readonly Messenger Instance = new Messenger();
-
-        private List<Action<string>> _callbacks = new List<Action<string>>();
-
-        public void Subscribe(Action<string> callback)
-        {
-            _callbacks.Add(callback);
-        }
-
-        public void Unsubscribe(Action<string> callback)
-        {
-            _callbacks.Remove(callback);
-        }
-
-        public void Publish(string message)
-        {
-            foreach (var callback in _callbacks)
-            {
-                callback.Invoke(message);
-            }
-        }
-    }
-
     public class Subscriber
     {
         private string _name;
@@ -36,7 +11,7 @@ namespace WeakSubscription
         public Subscriber(string name)
         {
             _name = name;
-            Messenger.Instance.Subscribe(m => Console.WriteLine("{0} received \"{1}\"", _name, m));
+            MessagingCenter.Subscribe<object>(this, "Hello", _ => Console.WriteLine("{0} received", _name));
         }
 
         public string Name { get { return _name; } }
@@ -46,15 +21,17 @@ namespace WeakSubscription
     {
         private static void Main(string[] args)
         {
+            object sender = new object();
+
             var s1 = new Subscriber("s1");
             new Subscriber("s2");
 
-            Messenger.Instance.Publish("Hello");
+            MessagingCenter.Send(sender, "Hello");
 
             GC.Collect();
             GC.WaitForFullGCComplete();
 
-            Messenger.Instance.Publish("World");
+            MessagingCenter.Send(sender, "Hello");
         }
     }
 }
